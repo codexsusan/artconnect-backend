bdc6097ee651613baed7d39e0ee90f47364fb6e3
codexsusan <susankhadka708@gmail.com>
2024-02-22 15:43:55 +0530

    Fixed otp issue

--- models/admin.model.ts
+++ models/admin.model.ts
@@ -1,48 +1,46 @@
 import {Model, model, Schema} from "mongoose";
 import {AdminInterface} from "../types";
 
-const adminModel: Schema<AdminInterface> = new Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        adminName: {
-            type: String,
-            required: true,
-        },
-        contact: {
-            type: String,
-            required: true,
-        },
-        location: {
-            type: String,
-            required: false,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        profilePicture: {
-            type: String,
-            required: false,
-        },
-        isVerified: {
-            type: Boolean,
-            required: true,
-            default: false
-        },
-        otp: {
-            type: String,
-            required: true,
-        },
-    },
-)
+const adminModel: Schema<AdminInterface> = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  adminName: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+    required: false,
+  },
+  isVerified: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  otp: {
+    type: String,
+    required: false,
+  },
+});
 
 const Admin: Model<AdminInterface> = model("Admin", adminModel)
 export default Admin;
