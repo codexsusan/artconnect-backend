d4e23839daed7dac97e114e237cebcb2e00e3878
codexsusan <susankhadka708@gmail.com>
2024-04-03 18:54:55 +0530

    Added notification route issue

--- routes/notification.routes.ts
+++ routes/notification.routes.ts
@@ -1,9 +1,13 @@
 import express, { Router } from "express";
 
 import { verifyToken } from "../middlewares/auth.middleware";
-import { fetchUserLatestNotifications } from "../controller/notification.controller";
+import {
+  fetchUserLatestNotifications,
+  pushNotificationHandler,
+} from "../controller/notification.controller";
 const router: Router = express.Router();
 
 router.get("/fetch/latest", verifyToken, fetchUserLatestNotifications);
+router.post("/push/:receiverId", verifyToken, pushNotificationHandler);
 
 export default router;
